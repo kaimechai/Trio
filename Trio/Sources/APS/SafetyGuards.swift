6a805701db3a4de1b24d20e1625bb056da377696
kaimechai <kweitz04@gmail.com>
2026-03-08 16:20:13 -0400

    Clean Restart Safety Guards

---
+++ Trio/Sources/APS/SafetyGuards.swift
@@ -0,0 +1,6 @@
+//
+//  SafetyGuards.swift
+//  Trio
+//
+//  Created by Kailea Weitz on 2026-03-07.
+//
---
+++ Trio/Sources/APS/SafetyGuardsConfig.swift
@@ -0,0 +1,6 @@
+//
+//  SafetyGuardsConfig.swift
+//  Trio
+//
+//  Created by Kailea Weitz on 2026-03-07.
+//
